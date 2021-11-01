8b76fe9421f3c83253beb8795a27c47824f17761
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-01 18:02:00 -0300

    Center title and remove search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import Search from './Search'
 
 const Header = () => {
   return (
-    <View style={styles.header}>
-      <Text style={styles.title}>Cryptos</Text>
-      <Search />
-    </View>
+    <Text style={styles.title}>Cryptos</Text>
   )
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row', justifyContent: 'space-between', width: '90%', marginBottom: 20
-  },
   title: {
     color: '#fff', marginTop: 60, fontSize: 20
   },
