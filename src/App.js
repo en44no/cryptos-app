401747c77e1d9ad0256fce40a2960f01141b2b96
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-01 17:10:51 -0300

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StatusBar, StyleSheet } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor='#141414' />
+      <Header />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#141414', alignItems: 'center', flex: 1,
   },
 });
