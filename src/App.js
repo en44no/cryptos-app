e628e5add447957f17af33bb387b925014ed3076
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-01 18:01:25 -0300

    Fetch data from CoinGecko and list coins

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StatusBar, StyleSheet } from 'react-native';
 import Header from './components/Header';
+import ListCoins from './components/ListCoins';
 
 export default function App() {
+  const [coins, setCoins] = useState([]);
+
+  const loadData = async () => {
+    const cryptosResponse = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    const cryptos = await cryptosResponse.json();
+    setCoins(cryptos);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor='#141414' />
       <Header />
+      <ListCoins coins={coins} />
     </View>
   );
 }
